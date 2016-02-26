21b60ebe7030dbc88b2fe9d67088180396b7a1e5
chrisk <ckv9508@saintjoe.edu>
2016-02-26 14:40:01 -0500

    Christopher Kenessey 6 times

--- TestOneCMK/src/edu/saintjoe/cs/chrisk/TestOneCMK/TestOneCMK.java
+++ TestOneCMK/src/edu/saintjoe/cs/chrisk/TestOneCMK/TestOneCMK.java
@@ -5,6 +5,12 @@ public class TestOneCMK {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
+		System.out.println("Christopher Kenessey");
+		System.out.println("Christopher Kenessey");
+		System.out.println("Christopher Kenessey");
+		System.out.println("Christopher Kenessey");
+		System.out.println("Christopher Kenessey");
+		System.out.println("Christopher Kenessey");
 	}
 
 }
